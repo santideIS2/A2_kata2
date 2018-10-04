7318385b3705abfc917b6009d27504641d5c9ad6
Usuario <Usuario@10.22.148.97>
2018-10-04 12:30:51 +0100

    Kata2 4.0

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -1,33 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata2;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author Usuario
- */
-public class Histogram {
-    private final int[] data;
+public class Histogram<T> {
+    private final T[] data;
 
-    public int[] getData() {
+    public T[] getData() {
         return data;
     }
 
-    public Histogram(int[] data) {
+    public Histogram(T[] data) {
         this.data = data;
     }
     public Map getHistogram(){
-        Map<Integer, Integer> histogram = new HashMap<>();
-        
-        for (int i : data) {
+        Map<T, Integer> histogram = new HashMap<>();
+        for (T i : data) {
             histogram.put(i,histogram.containsKey(i) ? histogram.get(i)+ 1 : 1);
-        }
+        } 
         return histogram;
     }
 }
